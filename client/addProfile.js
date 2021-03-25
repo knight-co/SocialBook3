e811b04278240aa41af5d00c8721889b644c7d04
justbraf <dwaynecorey+github@gmail.com>
2021-03-25 16:11:41 -0400

    Updated add user UI and added validtion

--- client/addProfile.js
+++ client/addProfile.js
@@ -1,16 +1,42 @@
 Template.addProfile.events({
     'click .js-save'(event){
+        // grabs data from html page
+        let profPic = $('#profPic').val();
         let profFirst = $('#firstName').val();
         let profLast = $('#lastName').val();
         let profAge = $('#age').val();
-        //console.log(profFirst, profLast, profAge);
-        profilesdb.insert({
-            "pFirst":profFirst,
-            "pLast":profLast,
-            "pAge":profAge
-        });
-        $('#firstName').val("");
-        $('#lastName').val("");
-        $('#age').val("");
+        // reset each input box
+        $("#profPic").removeClass("invalidWarn");
+        $("#firstName").removeClass("invalidWarn");
+        $("#lastName").removeClass("invalidWarn");
+        $("#age").removeClass("invalidWarn");
+        if (profPic == "")
+            $("#profPic").addClass("invalidWarn");
+        else if (profFirst == "")        
+            $("#firstName").addClass("invalidWarn");
+        else if (profLast == "")
+            $("#lastName").addClass("invalidWarn");
+        else if (profAge == "")
+            $("#age").addClass("invalidWarn");
+        else {
+            // Save data into collection
+            profilesdb.insert({
+                "pPic":profPic,
+                "pFirst":profFirst,
+                "pLast":profLast,
+                "pAge":profAge
+            });
+            // clear input boxes
+            document.getElementById("preImg").src = "blank-profile_640.png";
+            $('#profPic').val("");
+            $('#firstName').val("");
+            $('#lastName').val("");
+            $('#age').val("");
+            $('#addModal').modal('hide');
+        }
+    },
+    'input #profPic'(event){
+        let profPic = $('#profPic').val();
+        document.getElementById("preImg").src = profPic;
     }
 });
