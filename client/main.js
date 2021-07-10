6e335334519a12c531601044847f4143f891473e
Samson <smwsamson@outlook.com>
2021-07-10 14:59:55 -0400

    Delete config and user Edits

--- client/main.js
+++ client/main.js
@@ -2,12 +2,14 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import './main.html';
 import './myAccounts.html';
 import './addProfile.html';
+import './confirmDel.html';
+import './editProfile.html';
 
 import '../lib/collection.js';
 import './myAccounts.js';
 import './addProfile.js';
+import './editProfile.js';
--- client/myAccounts.js
+++ client/myAccounts.js
@@ -3,14 +3,62 @@ Template.myAccounts.helpers({
         return profilesdb.find();
     }
 });
+
 Template.myAccounts.events({
     'click .js-delete'(event){
-        let myId = this._id;
-        let myDoc = event.target.parentnode.parentnode.parentnode;
-        $(myDoc).fadeOut( "slow", function(){
-            //delete document
-            profilesdb.remove({_id: myId});
+        let myID = this._id;
+        $("#confirmID").val(myID);
+        $('#confirmModal').modal('show');
+    },
+    
+    'click .js-confirmDel'(event){
+        let delID = $("#confirmID").val();
+        $('#confirmModal').modal('hide');
+        $("#"+delID).fadeOut( "slow", function(){
+            profilesdb.remove({_id: delID});
         });
-        console.log("delete", myId);
+        
+    },
+
+    'click .js-edit'(event){
+        let myID = this._id;
+        $("#editID").val(myID);
+        let imgPath = this.pPic;
+        let fname = this.pFirst;
+        let lname = this.pLast;
+        let age = this.pAge;
+        let gender = this.pGen;
+        document.getElementById("editImg").src = imgPath;
+       $('#editPic').val(imgPath);
+        $('#editfirstName').val(fname);
+        $('#editlastName').val(lname);
+       $('#editage').val(age);
+        $('#editModal').modal('show');
+        let genRadio = $('#editModal input[name="genderRadio"]');
+        if (gender === "male")
+         genRadio.filter('[value=male]').prop('checked', true);
+        else
+        genRadio.filter('[value=female]').prop('checked', true);
+
+    },
+    'click .js-saveEdit'(event){
+        let editID = $("#editID").val();
+        let imgPath = $('#editPic').val();
+        let fname = $('#editfirstName').val();
+        let lname = $('#editlastName').val();
+       let age = $('#editage').val();
+       let gender = $('#editModal input[name="genderRadio"]:checked').val();
+       $('#editModal').modal('hide');
+       profilesdb.update({_id: editID},
+            {$set:{
+                'pPic': imgPath,
+                'pFirst': fname,
+                'pLast': lname,
+                'pAge': age,
+                'pGen': gender
+
+            }}
+        );
+
     }
 });
